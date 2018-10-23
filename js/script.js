4e9a614a100a59417d7533c2ecec3b46bb644313
drunkfurball <10695583+drunkfurball@users.noreply.github.com>
2018-10-23 11:00:35 -0500

    add moon phase indicator

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const FPS = 30; // number of updates per second
+const MOON_X = 100;
+const MOON_Y = 60;
+const MOON_SCALE = 0.45;
 const CLOCK_RADIUS = 100; // Sets the size of the clock
 const DAYS_OF_THE_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // array of days for word calendar
 const DAYS_ABBREVIATED = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -33,6 +36,8 @@ let label_dragging = false;
 let pen_selecting = false;
 let rightnow = new Date();
 let todo_list = [];
+let moon_toggle = true;
+let flip_the_moon = false;
 let calendar_toggle = true;
 let clock_toggle = true;
 let radius = CLOCK_RADIUS * 0.90;
@@ -40,24 +45,20 @@ let labels = [];
 let pens = [];
 let yyyy = rightnow.getFullYear();
 
-//sets up the labels
-labels.push({boxes: []});
+labels.push({boxes: []});// set up the labels
 for (let i = 0; i < DAYS_OF_THE_WEEK.length; i++) {
     labels[0].boxes.push({name: DAYS_OF_THE_WEEK[i], drag: false, x: (i * 129) + 5, y: 600, default_x: (i * 129) + 5, default_y: 600, w: 125, h: 20});
 }
-
 labels.push({boxes: []});
 for (let i = 0; i < MONTHS_OF_THE_YEAR.length; i++) {
     labels[1].boxes.push({name: MONTHS_OF_THE_YEAR[i], drag: false, x: (i * 89) + 5, y: 625, default_x: (i * 89) + 5, default_y: 625, w: 85, h: 20});
 }
-
 labels.push({boxes: []});
 for (let i = 0; i < DAYS_OF_THE_MONTH.length; i++) {
     labels[2].boxes.push({name: DAYS_OF_THE_MONTH[i], drag: false, x: (i * 34) + 5, y: 650, default_x: (i * 34) + 5, default_y: 650, w: 30, h: 20});
 }
 
-//sets up the pens
-for (let i = 0; i < COLORS[scheme_index].length; i++) {
+for (let i = 0; i < COLORS[scheme_index].length; i++) { //set up the pens
     if (COLORS[scheme_index][i] != DEFAULT_BACKGROUND) {
         pens.push({color: i, pixels: []});
     }
@@ -82,6 +83,12 @@ function updateToDo() {
 function addTask(descrip) {
     let task;
     switch(descrip) {
+        case "flip the moon":
+            flipMoon();
+            break;
+        case "moon":
+            toggleMoon();
+            break;
         case "clock":
             toggleClock();
             break;
@@ -91,7 +98,7 @@ function addTask(descrip) {
         case "help":
             task = {
                 color: (selected_pen == 7? 6: selected_pen),
-                description: "Welcome to the Smart Board by Murph Strange<br /><br />Type <b>clock</b> to enable/disable the clock display.<br />Type <b>calendar</b> to enable/disable the calendar display.<br />Type <b>help</b> to display this helpful message.<br />Type anything else to add a task to the to do list. Double-click an item on the to do list to remove it."
+                description: "Welcome to the Smart Board by Murph Strange<br /><br />Type <b>clock</b> to enable/disable the clock display.<br />Type <b>moon</b> to enable/disable the moon phase indicator.<br />If you live in the Southern Hemisphere, and want the moon oriented to reflect your view of the moon, type <b>flip the moon</b>.<br />Type <b>calendar</b> to enable/disable the calendar display.<br />Type <b>help</b> to display this helpful message.<br />Type anything else to add a task to the to do list. Double-click an item on the to do list to remove it."
             }
             todo_list.push(task);
             document.getElementById("input").value = "";
@@ -111,6 +118,9 @@ function addTask(descrip) {
 function update() { //everything that needs to be drawn in every frame
     clearBoard();
     rightnow = new Date();
+    if (moon_toggle) {
+        drawMoon();
+    }
     if (clock_toggle) {
         drawClock();
     }
@@ -124,6 +134,14 @@ function update() { //everything that needs to be drawn in every frame
         drawWordCalendar();
         drawLabels();
     }
+}
+
+function toggleMoon() {
+    moon_toggle = !moon_toggle;
+}
+
+function flipMoon() {
+    flip_the_moon = !flip_the_moon;
 }
 
 function toggleClock() {
@@ -307,41 +325,32 @@ function sbDrop(event) {
     let msEvt = event;
     for (let i = 0; i < labels.length; i++) {
         for (let j = 0; j < labels[i].boxes.length; j++) {
-            //35, 200, 129, 24
             if (i == 0 && msEvt.offsetX > 35 && msEvt.offsetX < 164 && msEvt.offsetY > 200 && msEvt.offsetY < 224) {
-                //set all other labels in this set to their default x and y
-                if (labels[i].boxes[j].drag == false) {
+                if (labels[i].boxes[j].drag == false) { // set all other labels in this set to their default x and y
                     labels[i].boxes[j].x = labels[i].boxes[j].default_x;
                     labels[i].boxes[j].y = labels[i].boxes[j].default_y;
                 }
-                else {
-                    //set the dropped label centered in the placeholder box
+                else { // set the dropped label centered in the placeholder box
                     labels[i].boxes[j].x = 37;
                     labels[i].boxes[j].y = 202;
                 }
             }
-            //35, 228, 89, 24
             if (i == 1 && msEvt.offsetX > 35 && msEvt.offsetX < 124 && msEvt.offsetY > 228 && msEvt.offsetY < 252) {
-                //set all other labels in this set to their default x and y
-                if (labels[i].boxes[j].drag == false) {
+                if (labels[i].boxes[j].drag == false) { //set all other labels in this set to their default x and y
                     labels[i].boxes[j].x = labels[i].boxes[j].default_x;
                     labels[i].boxes[j].y = labels[i].boxes[j].default_y;
                 }
-                //set the dropped label centered in the placeholder box
-                else {
+                else { //set the dropped label centered in the placeholder box
                     labels[i].boxes[j].x = 37;
                     labels[i].boxes[j].y = 230;
                 }
             }
-            //130, 228, 34, 24
             if (i == 2 && msEvt.offsetX > 130 && msEvt.offsetX < 164 && msEvt.offsetY > 228 && msEvt.offsetY < 252) {
-                //set all other labels in this set to their default x and y
-                if (labels[i].boxes[j].drag == false) {
+                if (labels[i].boxes[j].drag == false) { //set all other labels in this set to their default x and y
                     labels[i].boxes[j].x = labels[i].boxes[j].default_x;
                     labels[i].boxes[j].y = labels[i].boxes[j].default_y;
                 }
-                //set the dropped label centered in the placeholder box
-                else {
+                else { //set the dropped label centered in the placeholder box
                     labels[i].boxes[j].x = 132;
                     labels[i].boxes[j].y = 230;
                 }
@@ -385,7 +394,7 @@ function sbMove(event) {
 }
 
 function makeCalendar(month=rightnow.getMonth()+1, year=rightnow.getFullYear()) {
-    let date = new Date(year, month -1, 1, 1, 1, 1, 1);
+    let date = new Date(year, month -1, 12, 1, 1, 1, 1);
     let search_index;
     if (date.getDate() > 7) {
         search_index = date.getDate() - date.getDay();
@@ -498,10 +507,151 @@ function drawCalendar() {
                     ctx.fillStyle = COLORS[scheme_index][4];
                 }
                 ctx.fillText(display[i][j], CALENDAR_ORIGIN.x + ((CALENDAR_DAY_WIDTH + 1) * j) + 1 + (CALENDAR_DAY_WIDTH/2), CALENDAR_ORIGIN.y + ((CALENDAR_DAY_WIDTH + 1) * i) + 1 + (CALENDAR_DAY_WIDTH/2));
-                
             }
         }
     }
+}
+
+function diffDays(first, second) {
+    return Math.abs(first - second)/86400000.0;
+}
+
+function moonPhase(month=rightnow.getMonth()+1, day=rightnow.getDate(), year=rightnow.getFullYear() ) {
+    let new_moon = new Date(2018, 9, 8, 3, 47, 1, 1);
+    let lum_percent = Math.round(((diffDays(new_moon, new Date(year, month-1, day, 12, 1, 1, 1))%29.530588853)/29.530588853) * 100);
+    if (new_moon > new Date(year, month-1, day, 12, 1, 1, 1)) {
+        lum_percent = 100 - lum_percent;
+    }
+    if (lum_percent <= 9) {
+        phase = "New";
+    }
+    else if (lum_percent > 9 && lum_percent < 23) {
+        phase = "Waxing Cresent";
+    }
+    else if (lum_percent >= 23 && lum_percent <= 35) {
+        phase = "First Quarter";
+    }
+    else if (lum_percent > 35 && lum_percent < 50) {
+        phase = "Waxing Gibbous";
+    }
+    else if (lum_percent >= 50 && lum_percent <= 59) {
+        phase = "Full";
+    }
+    else if (lum_percent > 59 && lum_percent < 75) {
+        phase = "Waning Gibbous";
+    }
+    else if (lum_percent >= 75 && lum_percent <= 85) {
+        phase = "Third Quarter";
+    }
+    else if(lum_percent > 85 && lum_percent <= 98) {
+        phase = "Waning Cresent";
+    }
+    else {
+        phase = "New";
+    }
+    return phase;
+}
+
+function drawMoon() {
+    let waxing_gibbous = ctx.createRadialGradient(MOON_X, MOON_Y, 50 * MOON_SCALE, MOON_X - 15, MOON_Y, 50* MOON_SCALE);
+    waxing_gibbous.addColorStop(0, "#333");
+    waxing_gibbous.addColorStop(1, "#fff");
+    let waning_gibbous = ctx.createRadialGradient(MOON_X, MOON_Y, 50* MOON_SCALE, MOON_X + 15, MOON_Y, 50* MOON_SCALE);
+    waning_gibbous.addColorStop(0, "#333");
+    waning_gibbous.addColorStop(1, "#fff");
+    let waxing_cresent = ctx.createRadialGradient(MOON_X, MOON_Y, 60* MOON_SCALE, MOON_X + 25, MOON_Y, 50* MOON_SCALE);
+    waxing_cresent.addColorStop(0, "#fff");
+    waxing_cresent.addColorStop(1, "#333");
+    let waning_cresent = ctx.createRadialGradient(MOON_X, MOON_Y, 60* MOON_SCALE, MOON_X - 25, MOON_Y, 50* MOON_SCALE);
+    waning_cresent.addColorStop(0, "#fff");
+    waning_cresent.addColorStop(1, "#333");
+    let full_m = ctx.createRadialGradient(MOON_X, MOON_Y, 60* MOON_SCALE, MOON_X, MOON_Y, 40* MOON_SCALE);
+    full_m.addColorStop(0, "#333");
+    full_m.addColorStop(1, "#fff");
+    let new_m = ctx.createRadialGradient(MOON_X, MOON_Y, 60* MOON_SCALE, MOON_X, MOON_Y, 40* MOON_SCALE);
+    new_m.addColorStop(0, "#fff");
+    new_m.addColorStop(1, "#333");
+    let third_quarter = ctx.createLinearGradient(MOON_X - (10 * MOON_SCALE), MOON_Y, MOON_X + (10 * MOON_SCALE), MOON_Y);
+    third_quarter.addColorStop(0, "#333");
+    third_quarter.addColorStop(1, "#fff");
+    let first_quarter = ctx.createLinearGradient(MOON_X - (10 * MOON_SCALE), MOON_Y, MOON_X + (10 * MOON_SCALE), MOON_Y);
+    first_quarter.addColorStop(0, "#fff");
+    first_quarter.addColorStop(1, "#333");
+    ctx.fillStyle = "#333";
+    ctx.beginPath();
+    ctx.arc(MOON_X, MOON_Y, 52 * MOON_SCALE, 0, Math.PI * 2, false);
+    ctx.fill();
+    let mm = rightnow.getMonth()+1;
+    for (let i = 0; i < labels[1].boxes.length; i++) {
+        if (labels[1].boxes[i].x == 37 && labels[1].boxes[i].y == 230) {
+            mm = i + 1;
+        }
+    }
+    let dd = rightnow.getDate();
+    for (let i = 0; i < labels[2].boxes.length; i++) {
+        if (labels[2].boxes[i].x == 132 && labels[2].boxes[i].y == 230) {
+            dd = i + 1;
+        }
+    }
+    switch(moonPhase(mm, dd, yyyy)) {
+        case "New":
+            ctx.fillStyle = new_m;
+            break;
+        case "Waxing Cresent":
+            if (flip_the_moon) {
+                ctx.fillStyle = waning_cresent;
+            }
+            else {
+                ctx.fillStyle = waxing_cresent;
+            }
+            break;
+        case "First Quarter":
+            if (flip_the_moon) {
+                ctx.fillStyle = third_quarter;
+            }
+            else {
+                ctx.fillStyle = first_quarter;
+            }
+            break;
+        case "Waxing Gibbous":
+            if (flip_the_moon) {
+                ctx.fillStyle = waning_gibbous;
+            }
+            else {
+                ctx.fillStyle = waxing_gibbous;
+            }
+            break;
+        case "Full":
+            ctx.fillStyle = full_m;
+            break;
+        case "Waning Gibbous":
+            if (flip_the_moon) {
+                ctx.fillStyle = waxing_gibbous;
+            }
+            else {
+                ctx.fillStyle = waning_gibbous;
+            }
+            break;
+        case "Third Quarter":
+            if (flip_the_moon) {
+                ctx.fillStyle = first_quarter;
+            }
+            else {
+                ctx.fillStyle = third_quarter;
+            }
+            break;
+        case "Waning Cresent":
+            if (flip_the_moon) {
+                ctx.fillStyle = waxing_cresent;
+            }
+            else {
+                ctx.fillStyle = waning_cresent;
+            }
+            break;
+    }
+    ctx.beginPath();
+    ctx.arc(MOON_X, MOON_Y, 50 * MOON_SCALE, 0, Math.PI * 2, false);
+    ctx.fill();
 }
 
 canv.onmousedown = sbDrag;
